53e82903e9440e595332f3e76da42567157d61f6
alvioa <alvioa8@gmail.com>
2024-05-01 01:18:46 -0800

    test2

--- Repositories/IRepository.cs
+++ Repositories/IRepository.cs
@@ -3,6 +3,9 @@ namespace TestingMVC.Repo
 {
     public interface IRepository<T> where T : class
     {
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
         public List<T> GetAll();
         public T GetById(string id);
         public void Insert(T obj);
