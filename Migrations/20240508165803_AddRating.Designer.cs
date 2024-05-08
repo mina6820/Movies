dcae0f68303e683c019dd07aa73771a8a8abe149
Mina Medhat <minamedhat123456@gmail.com>
2024-05-08 20:23:43 +0300

    Rating

--- Migrations/20240508165803_AddRating.Designer.cs
+++ Migrations/20240508165803_AddRating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Movies.Models;
 
@@ -11,9 +12,11 @@ using Movies.Models;
 namespace Movies.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240508165803_AddRating")]
+    partial class AddRating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
